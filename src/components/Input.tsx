f7cee8351c7f070e3837b023acb3d800d9e21153
YuyaKimura <hm20020@student.miyazaki-u.ac.jp>
2025-01-14 06:46:04 +0900

    fix: input の枠が入力領域以上に広がっていたため、修正

--- src/components/Input.tsx
+++ src/components/Input.tsx
@@ -7,7 +7,7 @@ export type InputProps = Omit<JSX.IntrinsicElements['input'], 'ref'> & {
 
 export const Input = ({ inputRef, ...props }: InputProps) => {
     return (
-        <div style={{ margin: 10 }} className='border border-gray-300 rounded-md w-1/4'>
+        <div style={{ margin: 10 }} className='border-2 border-gray-300 rounded-sm w-fit'>
             <input {...props} ref={inputRef} />
         </div>
     );
